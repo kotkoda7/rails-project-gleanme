33de32640ff32a6362f1cda7110da7c428dba4b5
kotkoda7 <christopherrobincox@Szilvias-MacBook-Air.local>
2018-09-26 15:39:53 -0700

    finalized sign up form and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :location_edibles
+  #resources :location_edibles
   resources :locations
   resources :users
-  resources :edibles
+  #resources :edibles
 
   root to: 'locations#home'
 
-  resources :users
+  #If you need to use a different controller namespace inside a namespace block you can specify an absolute controller path
+  get '/signup' => 'users#new'
+
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
