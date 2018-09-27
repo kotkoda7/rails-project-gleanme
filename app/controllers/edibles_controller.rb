a20b4df5df12c8203d18e7da7a52ad5f5a17ff55
kotkoda7 <christopherrobincox@Szilvias-MacBook-Air.local>
2018-09-26 18:53:57 -0700

    nav and form partial created

--- app/controllers/edibles_controller.rb
+++ app/controllers/edibles_controller.rb
@@ -1,2 +1,15 @@
 class EdiblesController < ApplicationController
+	  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  
+
+  def logged_in?
+    !!current_user
+  end
+
+  
+
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,4 +3,8 @@ class LocationsController < ApplicationController
 	def home
 		@locations = Location.all
 	end
+
+	def new
+		@location = Location.new
+	end
 end
