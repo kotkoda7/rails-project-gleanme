65737f8b74ec31f03dfd303191d72cbd5e64fae3
kotkoda7 <christopherrobincox@Szilvias-MacBook-Air.local>
2018-10-09 13:59:52 -0700

    fixing (nested resources) locations controller edit and new views

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,9 @@
 class LocationsController < ApplicationController
-	before_action :ensure_login, only: [:new, :create, :edit, :update]
+	before_action :ensure_login, only: [:new, :create, :edit, :update, :destroy]
+
+	def home
+		#Welcome page
+	end
 
 	def index
 		if params[:user_id]
@@ -11,45 +15,61 @@ class LocationsController < ApplicationController
     	end
 	end
 
-	def home
-		#Welcome page
-	end
-
-
 	def show
 	 	@location = Location.find_by(params[:id])
 	 	#@edibles = @location.edibles
 	end
 
 	def new
-    	@location = Location.new(user_id: params[:user_id])
-    	#@userlocs = @location.users
+		if logged_in?
+			@location = Location.new(user_id: params[:user_id])
+		else
+			redirect_to locations_path, alert: "You must be logged in to create a location."
+		end
 	end
 
+#want to make sure location is created/updated as user's lcoation!
 	def create
 		@location = Location.new(location_params)
 		#@location.user = current_user
 		@location.save
-		redirect_to user_path(current_user)
-	end
-
-	def edit
-		@location = Location.find_by(params[:id])
+		redirect_to user_locations_path(current_user)
 	end
 
 	def update
-		#@location = Location.find_by(params[:id]
-		#@location.update(address: params[:address], lat: params[:lat], lng: params[:lng], description: params[:description], loc_type: params[:loc_type], location_edible: params[:location][:edible], user_id: current_user.id)
-		#redirect_to location_path(@location)
+		@user
+		@location = Location.find_by(params[:id]
+		@
+		@location.update(params.require(:location))
+		#(address: params[:address], lat: params[:lat], lng: params[:lng], description: params[:description], loc_type: params[:loc_type], location_edible: params[:location][:edible], user_id: current_user.id)
+		redirect_to location_path(@location)
 	end
 
-	def delete
+	def edit
+    	if logged_in?
+			@user = User.find_by(id: params[:user_id])
+			@location = @user.locations.find_by(id: params[:id])
+			redirect_to user_locations_path(current_user)
+    	else
+      		redirect_to locations_path, notice: "You can't edit someone else's location!"
+ 		end
 	end
+
+	def destroy
+    	@location = Location.find(params[:id])
+    	if logged_in?
+    		@location.destroy
+    		flash[:notice] = "Location deleted."
+    		redirect_to user_locations_path
+    	else
+    		flash.now[:notice] = "You cannot delete another user's location."
+    	end
+  	end
 
 
 	private
 
 	def location_params
-		params.require(:location).permit(:address, :lat, :lng, :description, :edible_ids => []) 
+		params.require(:location).permit(:address, :desscription, :lat, :lng, :description, :user_id, :user_name, :loc_type, :edible_ids => []) 
 	end
 end
